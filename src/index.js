984eba8694d6fd91116ead25d1508b1ceb38bd8e
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-28 02:52:33 +0300

    Add Provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyle } from './GlobalStyle.jsx';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import 'modern-normalize';
+import { App } from 'components/App';
 import { theme } from './constants/theme';
+import { store } from './redux/store';
+import { GlobalStyle } from './GlobalStyle.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+        <GlobalStyle />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
