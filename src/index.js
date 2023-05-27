0e437918bbdc0aab26a7682258b405df922f625c
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-28 00:40:35 +0300

    Add modern-normalize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle.jsx';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
+import 'modern-normalize';
 import { theme } from './constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
