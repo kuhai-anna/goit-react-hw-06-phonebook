a46b79d5d4f8da9d5882c1ea8ba808dccf6aabb1
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-28 18:14:48 +0300

    Add persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './filter/filterSlice';
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
 });
+
+const persistedRootReducer = persistReducer(rootPersistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedRootReducer,
+});
+
+export const persistor = persistStore(store);
